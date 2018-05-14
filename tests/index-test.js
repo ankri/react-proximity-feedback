572ca8bcaf5bccce950fc8dcf3272878231a94ea
ak <ak@tangro.de>
2018-05-15 00:42:58 +0200

    removed folder tests in favor of cypress

--- tests/index-test.js
+++
@@ -1,7 +0,0 @@
-import expect from 'expect';
-import React from 'react';
-import { render, unmountComponentAtNode } from 'react-dom';
-
-import Component from 'src/';
-
-describe('Component', () => {});
